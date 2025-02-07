182e42d9004bf1f5b54385bc23d01610fc309ac9
Taylor Markham <59581164+Taylordmark@users.noreply.github.com>
2025-02-07 11:48:39 -0800

    Delete MySurveyApp JSON directory

--- MySurveyApp JSON/main/win32_directx12/CreateSurveysPage.h
+++
@@ -1,8 +0,0 @@
-#ifndef CREATESURVEYSPAGE_H
-#define CREATESURVEYSPAGE_H
-
-namespace MyApp {
-    void RenderCreateSurveysPage();
-}
-
-#endif // CREATESURVEYSPAGE_H
--- MySurveyApp JSON/main/win32_directx12/HomePage.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#ifndef HOMEPAGE_H
-#define HOMEPAGE_H
-
-namespace MyApp {
-    void RenderHomePage();
-}
-
-#endif // HOMEPAGE_H
